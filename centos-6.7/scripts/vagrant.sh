b5f6ab5357b368bf4f200759b17c558ff4040484
TED Vortex (Teodor Eugen Dutulescu) <teodord@cpwonlinesolutions.com>
2015-10-02 22:01:12 +0100

    Further improvements, tested all providers

--- centos-6.7/scripts/vagrant.sh
+++ centos-6.7/scripts/vagrant.sh
@@ -5,3 +5,6 @@ mkdir -pm 700 /home/vagrant/.ssh
 curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
+
+groupadd www-data
+useradd -g www-data www-data
