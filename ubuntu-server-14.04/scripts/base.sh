dbd514ed1198adafce298c5fd5162c991a377a61
TED Vortex (Teodor Eugen Dutulescu) <teodord@cpwonlinesolutions.com>
2015-11-29 20:34:32 +0000

    Updated box with git

--- ubuntu-server-14.04/scripts/base.sh
+++ ubuntu-server-14.04/scripts/base.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 apt-get update
 apt-get -y upgrade
-apt-get -y install linux-headers-$(uname -r) build-essential
+apt-get -y install linux-headers-$(uname -r) build-essential perl git
 
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
 sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
